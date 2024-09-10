85e3460e3c3e8ee90cba20424b169527fe2047a6
Arslan Abid <163387090+Arslanabid09@users.noreply.github.com>
2024-09-11 03:36:33 +0500

    Update Weather.jsx

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -89,7 +89,7 @@ const Weather = () => {
 
         {weatherData ? (
           <div className='flex flex-col space-y-10 items-center mt-10'>
-            <img src={`./${weatherData.weatherIcon}`} alt="Weather Icon" className='w-24' />
+            <img src={`${.weatherData.weatherIcon}`} alt="Weather Icon" className='w-24' />
             <div className='text-center'>
               <h1 className='text-6xl text-white font-bold'>{Math.round(weatherData.temp)}°C</h1>
               <p className='mt-2 text-white font-semibold text-xl'>{weatherData.city}, {weatherData.country}</p>
